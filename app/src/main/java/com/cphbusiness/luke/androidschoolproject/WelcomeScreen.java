81f267c6dd29a3b89a1ff9a8635f6147f174f29c
biggiejr <jrmacej@gmail.com>
2016-05-11 12:52:31 +0200

    Adding permissions Adding welcome screen layout with corresponding java class

---
+++ app/src/main/java/com/cphbusiness/luke/androidschoolproject/WelcomeScreen.java
@@ -0,0 +1,53 @@
+package com.cphbusiness.luke.androidschoolproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.ImageView;
+
+public class WelcomeScreen extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+
+
+
+        setContentView(R.layout.activity_welcome_screen);
+
+        /* adapt the image to the size of the display */
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        Bitmap bmp = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.mipmap.ic_launcher),size.x,size.y,true);
+
+        ImageView image = (ImageView) findViewById(R.id.WelcomeImage);
+        image.setImageBitmap(bmp);
+
+
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeScreen.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+
+
+
+}
