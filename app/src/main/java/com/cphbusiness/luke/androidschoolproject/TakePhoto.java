5967533b75415f4bb5a84cc00a74733691183895
biggiejr <jrmacej@gmail.com>
2016-05-12 09:53:06 +0200

    created TakePhoto layout & class

---
+++ app/src/main/java/com/cphbusiness/luke/androidschoolproject/TakePhoto.java
@@ -0,0 +1,15 @@
+package com.cphbusiness.luke.androidschoolproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.mato.dsdomibyg.R;
+
+public class TakePhoto extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_photo);
+    }
+}
