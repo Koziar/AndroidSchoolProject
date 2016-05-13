3a5f581dea42fb03fcc3d8f3ae181dbf27ed745c
LukaszKoziarski <Luke@k1ou2.local>
2016-05-13 13:55:52 +0200

    Population spinner and preparing for creating a TXT file
    also added checking if location services are enabled and handling it

--- app/src/main/java/com/cphbusiness/luke/androidschoolproject/SendActivity.java
+++ app/src/main/java/com/cphbusiness/luke/androidschoolproject/SendActivity.java
@@ -1,14 +1,22 @@
 package com.cphbusiness.luke.androidschoolproject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.dropbox.client2.DropboxAPI;
 import com.dropbox.client2.android.AndroidAuthSession;
 import com.dropbox.client2.session.AppKeyPair;
 import com.example.mato.dsdomibyg.R;
+
+import java.util.ArrayList;
 
 public class SendActivity extends Activity {
 
@@ -19,6 +27,10 @@ public class SendActivity extends Activity {
     private static final String APP_SECRET = "yel5utqc01prwak";
     private static final String ACCESSTOKEN = "VQp1oWrVDsAAAAAAAAAAB8hcszBhmPmO77vkQ2yq_t_PBVtBfwAPT56RnSYgrwQg";
     private DropboxAPI.UploadRequest request;
+    private ArrayList<String> data;
+    private LocationManager locationManager = (LocationManager)
+            getSystemService(Context.LOCATION_SERVICE);
+
 
     private AndroidAuthSession buildSession() {
         AppKeyPair appKeyPair = new AppKeyPair(APP_KEY, APP_SECRET);
@@ -34,12 +46,50 @@ public class SendActivity extends Activity {
 
         Button sendButton = (Button) findViewById(R.id.send_button);
         final EditText descriptionField = (EditText) findViewById(R.id.description_textfield);
-        descriptionField.getText().toString();
-
         AndroidAuthSession session = buildSession();
         dropboxAPI = new DropboxAPI<AndroidAuthSession>(session);
 
-        // ...
+        // Populating the spinner, in further version to be replaced with real names of folders from dropbox account
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_addresses);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.addresses_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+        sendButton.setOnClickListener(v -> {
+
+            // check if location services is enabled
+            if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                buildAlertMessageNoGps();
+            }
+
+            String description = descriptionField.getText().toString();
+            String userName = LoginActivity.getName();
+            String userPhone = LoginActivity.getPhone();
+
+            // ... In progress ...
+
+        });
+        // ... In progress ...
+
     }
 
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", (dialog, id) -> {
+                    startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                })
+                .setNegativeButton("No", (dialog, id) -> {
+                    dialog.cancel();
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
 }
