42c7b85b9f4016068aa0df9d4e8a3f513669a33e
LukaszKoziarski <lkoziars@gmail.com>
2016-05-20 10:18:44 +0200

    Readme file with account info
    also contains some new elements in Send Activity.java

--- app/src/main/java/com/cphbusiness/luke/androidschoolproject/SendActivity.java
+++ app/src/main/java/com/cphbusiness/luke/androidschoolproject/SendActivity.java
@@ -1,15 +1,13 @@
 package com.cphbusiness.luke.androidschoolproject;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Html;
+import android.os.Environment;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dropbox.client2.DropboxAPI;
@@ -17,6 +15,10 @@ import com.dropbox.client2.android.AndroidAuthSession;
 import com.dropbox.client2.session.AppKeyPair;
 import com.example.mato.dsdomibyg.R;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class SendActivity extends Activity {
@@ -33,7 +35,15 @@ public class SendActivity extends Activity {
 
     private Button sendButton;
     private EditText descriptionField;
-    private TextView testTextView; // to be deleted later
+//    private TextView testTextView; // to be deleted later
+
+    private String description;
+    private String userName;
+    private String userPhone;
+    private String linkToGoogleMaps;
+    private String address;
+
+    private String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/toDropbox";
 
     GPSTracker gps;
 
@@ -44,12 +54,14 @@ public class SendActivity extends Activity {
 
         sendButton = (Button) findViewById(R.id.send_button);
         descriptionField = (EditText) findViewById(R.id.description_textfield);
+        File dir = new File(path);
+        dir.mkdirs();
 
         AndroidAuthSession session = buildSession();
         dropboxAPI = new DropboxAPI<AndroidAuthSession>(session);
 
         // Populating the spinner, in further version to be replaced with real names of folders from dropbox account
-        Spinner spinner = (Spinner) findViewById(R.id.spinner_addresses);
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner_addresses);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.addresses_array, android.R.layout.simple_spinner_item);
@@ -63,23 +75,49 @@ public class SendActivity extends Activity {
             public void onClick(View v) {
                 gps = new GPSTracker(SendActivity.this);
 
+                description = descriptionField.getText().toString();
+                userName = LoginActivity.getName();
+                userPhone = LoginActivity.getPhone().toString();
+                Spinner sp = (Spinner) findViewById(R.id.spinner_addresses);
+                address = sp.getSelectedItem().toString();
+
                 if (gps.canGetLocation()) {
                     double latitude = gps.getLatitude();
                     double longitude = gps.getLongitude();
-                    String linkToGoogleMaps = "https://www.google.com/maps/place/" + latitude + "," + longitude;
-                    testTextView = (TextView) findViewById(R.id.testTextView);
-                    testTextView.setTextColor(Color.BLACK);
-                    testTextView.setText(Html.fromHtml("<a href=" + linkToGoogleMaps + ">See in Google Maps</a> "));
+                    linkToGoogleMaps = "https://www.google.com/maps/place/" + latitude + "," + longitude;
+//                    testTextView = (TextView) findViewById(R.id.testTextView);
+//                    testTextView.setTextColor(Color.BLACK);
+//                    testTextView.setText(Html.fromHtml("<a href=" + linkToGoogleMaps + ">See in Google Maps</a> "));
                     Toast.makeText(getApplicationContext(),
                             "Your location is: \nLat: " + latitude + "\nLong: " + longitude,
                             Toast.LENGTH_LONG).show();
                 } else {
                     gps.showSettingsAlert();
                 }
+
+                data.add(userName);
+                data.add(userPhone);
+                data.add(description);
+                data.add(linkToGoogleMaps);
+
+                File txtFile = new File(path + "/testFile.txt");
+
+                try {
+                    save(txtFile, data);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
     } // end of onCreate
+
+    public void save(File fileName, ArrayList<String> data) throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter(new FileOutputStream(fileName));
+        for (String d : data)
+            pw.println(d);
+        pw.close();
+    }
 
     private AndroidAuthSession buildSession() {
         AppKeyPair appKeyPair = new AppKeyPair(APP_KEY, APP_SECRET);
