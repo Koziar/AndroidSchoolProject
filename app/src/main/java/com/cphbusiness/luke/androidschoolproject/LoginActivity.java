27ae2f86acd0b3cabd69df7ee4db752c1421fea6
biggiejr <jrmacej@gmail.com>
2016-05-25 12:44:08 +0200

    exception handling

--- app/src/main/java/com/cphbusiness/luke/androidschoolproject/LoginActivity.java
+++ app/src/main/java/com/cphbusiness/luke/androidschoolproject/LoginActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.mato.dsdomibyg.R;
 
@@ -27,9 +28,16 @@ public class LoginActivity extends Activity {
 
                 final EditText nameField = (EditText) LoginActivity.this.findViewById(R.id.userName);
                 name = nameField.getText().toString();
+                if(name.matches("")){
+                    Toast.makeText(getApplicationContext(), "Enter your name",Toast.LENGTH_LONG ).show();
+                    return;
+                }
                 final EditText phoneField = (EditText) LoginActivity.this.findViewById(R.id.userPhone);
                 phone = phoneField.getText().toString();
-
+                if(phone.matches("")){
+                    Toast.makeText(getApplicationContext(), "Enter your telephone number",Toast.LENGTH_LONG ).show();
+                    return;
+                }
                 Intent intent = new Intent(LoginActivity.this, TakePhoto.class);
                 LoginActivity.this.startActivity(intent);
             }
